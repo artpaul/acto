b5aaec7fea15142eea015995094d7a5d6d794a7e
artpaul <world.stan@gmail.com>
2009-06-14 12:53:43 +0000

    atomic for x64 linux

--- multiprog/system/atomic.h
+++ multiprog/system/atomic.h
@@ -33,7 +33,32 @@
     } // namespace acto
 
 #elif defined(__GNUC__) 
-#   if !defined(__ia64__)
+#   if defined (__x86_64__) || defined(__ia64__)
+
+        namespace acto {
+            typedef volatile long   atomic_t;
+
+            inline long atomic_add(atomic_t* const a, long b) {
+                return __sync_add_and_fetch(a, b);
+            }
+
+            inline long atomic_increment(atomic_t* const a) {
+                return __sync_add_and_fetch(a, 1);
+            }
+
+            inline long atomic_decrement(atomic_t* const a) {
+                return __sync_sub_and_fetch(a, 1);
+            }
+
+            inline bool atomic_compare_and_swap(atomic_t* const a, long val, long cmp) {
+                return __sync_bool_compare_and_swap(a, cmp, val);
+            }
+
+            inline long atomic_swap(atomic_t* const a, long b) {
+                return __sync_lock_test_and_set(a, b);
+            }
+        } // namespace acto
+#   else
         namespace acto {
             typedef volatile long   atomic_t;
 
@@ -43,7 +68,7 @@
                 __asm__ __volatile__(
                     "lock\n\t"
                     "xadd %0, %1\n\t"
-                    : "+r" (tmp), "+m" (a)
+                    : "+r" (tmp), "+m" (*(a))
                     :
                     : "memory");
 
@@ -78,7 +103,7 @@
                 __asm__ __volatile__ (
                     "lock\n\t"
                     "xchg %0, %1\n\t"
-                    : "+m" (a) , "+q" (rv)
+                    : "+m" (*(a)) , "+q" (rv)
                     :
                     : "cc");
 
